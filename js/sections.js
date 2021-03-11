dd1402c550abc0cac7e513376fa4bcb4fe242a4e
jcalderita <75628230+jcalderita@users.noreply.github.com>
2021-03-11 09:37:53 +0100

    Section hidden y observer.

--- js/sections.js
+++ js/sections.js
@@ -7,10 +7,15 @@ const revelarSections = (entries, observer) => {
   if (entry.isIntersecting) {
     entry.target.classList.remove('section--hidden');
     observer.unobserve(entry.target);
+    const up = entry.target.querySelector('.section__study-up');
+    if (!up) return;
+    up.classList.add('section__study-up--animation');
+    const down = entry.target.querySelector('.section__study-down');
+    down.classList.add('section__study-down--animation');
   }
 };
 
-const observer = new IntersectionObserver(revelarSections, { threshold: 0.15 });
+const observer = new IntersectionObserver(revelarSections, { threshold: [0.15, 0.25, 0.5, 0.75, 1] });
 
 const inicializarSecciones = () => sections.forEach(section => observer.observe(section));
 
